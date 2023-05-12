3f2c80e260d4214e93a82e0cc89e221e9049d813
Aleksey Ivasyuta <avivasyuta@avito.ru>
2023-05-12 12:21:23 +0700

    replace xhook library with local copy

---
+++ src/xhook/index.js
@@ -0,0 +1,48 @@
+/* eslint-disable */
+import { EventEmitter } from './misc/event-emitter';
+import headers from './misc/headers';
+
+// patchable types
+import XMLHttpRequest from './patch/xmlhttprequest';
+import fetch from './patch/fetch';
+
+// global state
+import hooks from './misc/hooks';
+// the global hooks event emitter is also the global xhook object
+// (not the best decision in hindsight)
+const xhook = hooks;
+xhook.EventEmitter = EventEmitter;
+// modify hooks
+xhook.before = function (handler, i) {
+    if (handler.length < 1 || handler.length > 2) {
+        throw 'invalid hook';
+    }
+    return xhook.on('before', handler, i);
+};
+xhook.after = function (handler, i) {
+    if (handler.length < 2 || handler.length > 3) {
+        throw 'invalid hook';
+    }
+    return xhook.on('after', handler, i);
+};
+
+// globally enable/disable
+xhook.enable = function () {
+    XMLHttpRequest.patch();
+    fetch.patch();
+};
+xhook.disable = function () {
+    XMLHttpRequest.unpatch();
+    fetch.unpatch();
+};
+// expose native objects
+xhook.XMLHttpRequest = XMLHttpRequest.Native;
+xhook.fetch = fetch.Native;
+
+// expose helpers
+xhook.headers = headers.convert;
+
+// enable by default
+xhook.enable();
+
+export default xhook;
---
+++ src/xhook/misc/hooks.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+import { EventEmitter } from './event-emitter';
+
+// global set of hook functions,
+// uses event emitter to store hooks
+const hooks = EventEmitter(true);
+
+export default hooks;
