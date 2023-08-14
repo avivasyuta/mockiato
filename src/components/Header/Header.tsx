fd221b6853c2a6201ed0b2cfcfbae3e13eb4e55f
avivasyuta <avivasyuta@avito.ru>
2023-08-14 22:48:31 +0400

    fix: dark theme for header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,17 @@
 import React, { FC } from 'react';
+import { Box, useMantineTheme } from '@mantine/core';
 import styles from './Header.module.css';
 
-export const Header: FC<React.PropsWithChildren> = ({ children }) => (
-    <div className={styles.header}>
-        {children}
-    </div>
-);
+export const Header: FC<React.PropsWithChildren> = ({ children }) => {
+    const theme = useMantineTheme();
+
+    return (
+        <Box
+            className={styles.header}
+            bg={theme.colorScheme === 'dark' ? theme.colors.dark[6] : '#ffffff'}
+            style={{ borderColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2] }}
+        >
+            {children}
+        </Box>
+    );
+};
